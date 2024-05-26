864f0f3a6fafaa25dc8f30939f32a8374ab25eb7
ShaikShahed12 <shaddumusaib8@gmail.com>
2024-05-26 14:53:01 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,11 +2,14 @@
 import { Alert, Button, Label, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInFailure, signInStart, signInSuccess } from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false); // Changed 'loadin' to 'loading'
+
+  const {loading, error: errorMessage} = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -16,12 +19,13 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields');
+      return dispatch(signInFailure('Please fill out all fields'))
     }
 
-    setLoading(true); // Set loading state to true on form submission
+     // Set loading state to true on form submission
 
     try {
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -31,18 +35,18 @@ export default function SignIn() {
       const data = await res.json();
 
       if (data.success === false) {
-        setErrorMessage(data.message);
+        dispatch(signInFailure(data.message))
       }
 
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      console.error('Error:', error);
-      setErrorMessage('Something went wrong. Please try again.');
+      dispatch(signInFailure(error.message));
     }
 
-    setLoading(false); // Reset loading state after request completes
+    // Reset loading state after request completes
   };
 
   return (
