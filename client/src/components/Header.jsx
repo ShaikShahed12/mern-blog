f48fd94f13badca5f7b9cf2f5350673470b212b3
ShaikShahed12 <shaddumusaib8@gmail.com>
2024-05-29 17:25:28 +0530

    add preinstallation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,11 +4,30 @@ import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 import "../components/Header.css";
+import { signoutSuccess } from '../redux/user/userSlice';
 import { useSelector, useDispatch } from 'react-redux';
 
 export default function Header() {
   const path = useLocation().pathname;
   const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  
   return (
     <Navbar className="border-b-2">
       <Link
@@ -57,7 +76,7 @@ export default function Header() {
              <Dropdown.Item>Profile</Dropdown.Item>
            </Link>
            <Dropdown.Divider />
-           <Dropdown.Item>Sign out</Dropdown.Item>
+           <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
          </Dropdown>
         ):
         (
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,8 +10,6 @@ export default defineConfig({
         secure: false,
       },
     },
-
-
   },
   plugins: [react()],
-})
+});
